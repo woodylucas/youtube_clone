fd20d5be8c741d51979517e69a8cbf56b8e94bdb
woodylucas <wlucas0311@gmail.com>
2020-11-30 21:54:27 -0500

    create components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Search Bar</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
