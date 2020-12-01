731cccba6c66ccd814218f90ccab59338af47373
woodylucas <wlucas0311@gmail.com>
2020-11-30 22:22:34 -0500

    implemented a controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ term: evt.target.value });
+  }
   render() {
     return (
-      <div>
-        <h1>Search Bar</h1>
+      <div className="search-bar ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
