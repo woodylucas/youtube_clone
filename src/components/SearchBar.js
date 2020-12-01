2d20f9d37c2f1a5684d0e3b11e9c69e938d37c6d
woodylucas <wlucas0311@gmail.com>
2020-12-01 09:32:33 -0500

    added a form submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,15 +5,23 @@ class SearchBar extends Component {
     super(props);
     this.state = { term: "" };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit(this);
   }
 
   handleChange(evt) {
     this.setState({ term: evt.target.value });
   }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    // TODO:
+    // Make sure we call, callback from parent component
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form action="" className="ui form">
+        <form onSubmit={this.handleSumit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
