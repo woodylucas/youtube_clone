40c43d8b89c33149942bc624e58163a46beaba13
woodylucas <wlucas0311@gmail.com>
2020-12-01 20:07:49 -0500

    added a key to all items on list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, handleClick }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem handleClick={handleClick} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        handleClick={handleClick}
+        video={video}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
